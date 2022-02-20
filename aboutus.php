5a9b9478b03590eadfe0ba1553b903c7d95e88e5
Rashone <rashone911@hotmail.com>
2022-02-20 15:33:41 -0400

    Created about us page

---
+++ aboutus.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-5.1.3-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+    <title>About Us</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="navbarLinks" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarLinks">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a href="index.php" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="aboutus.php" class="nav-link">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a href="ordertaxi.php" class="nav-link">Taxi</a>
+                </li>
+                <li class="nav-item">
+                    <a href="contact.php" class="nav-link">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </nav> 
+    <h1>Welcome to islandMovers</h1>
+    <p>islandMovers is a taxi service that will take you anywhere you want to go on the beautiful island of St. Vincent.</p>
+</body>
+</html>
