08a18d74abc46f327eb98bf6fbccf40522e96c2d
Rashone <rashone911@hotmail.com>
2022-04-12 19:55:07 -0400

    Added API

---
+++ CalculationAPI/database/migrations/2022_04_09_192019_create_calculate_costs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCalculateCostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calculate_costs', function (Blueprint $table) {
+            $table->id();
+            $table->string('email')->unique();
+            $table->integer('cellphone');
+            $table->decimal('cost', $precision =  8, $scale = 2);
+            $table->string('location');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calculate_costs');
+    }
+}
